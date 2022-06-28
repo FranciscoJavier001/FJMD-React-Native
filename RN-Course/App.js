81ce2c248709d172917a01292179116b051dd494
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-28 18:51:28 -0500

    Styles ReactNative

--- RN-Course/App.js
+++ RN-Course/App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() { //** Esto es un FC */
   return (
-    <View style={styles.container}>
-      {/* Se metio texto a la App desde Expo Go */}
-      <Text>Hello World by Francisco Javier!!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <View>
+          <TextInput placeholder='Your course goal!' />
+          <Button title="Add Goal" />
+        </View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: {
+    padding: 50
+  }
 });
